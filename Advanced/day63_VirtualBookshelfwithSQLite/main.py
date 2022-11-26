45269986673a456693fcda003792841b26abb719
Jing <jing.lu.cn@outlook.com>
2022-11-26 19:55:12 +0000

    update

--- Advanced/day63_VirtualBookshelfwithSQLite/main.py
+++ Advanced/day63_VirtualBookshelfwithSQLite/main.py
@@ -1,79 +1,64 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
-# ----------------------- DB Setup --------------------- #
-# create the app
+# ------- Create app--------#
 app = Flask(__name__)
-# create the extension
-db = SQLAlchemy()
-# configure the SQLite database, relative to the app instance folder
+
+# ------- Connect to database --------#
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
-# initialize the app with the extension
-db.init_app(app)
+db = SQLAlchemy(app)
 
 
-# Create table
+# ------- Create table --------#
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(250), nullable=False, unique=True)
+    title = db.Column(db.String(250), nullable=False)
     author = db.Column(db.String(250), nullable=False)
     rating = db.Column(db.Float, nullable=False)
 
 
-# Create initial database
+# # ------- initial database --------#
 # with app.app_context():
 #     db.create_all()
 
-# ----------------------- Web Setup --------------------- #
+
 @app.route('/')
 def home():
-
-    with app.app_context():
-        all_books = db.session.query(Book).all()
+    all_books = db.session.query(Book).all()
+    print(type(all_books))
     return render_template('index.html', num=len(all_books), books=all_books)
+
+
+@app.route("/edit<int:id>", methods=['GET', 'POST'])
+def edit(id):
+    book_to_edit = Book.query.get(id)
+    if request.method == 'POST':
+        book_to_edit.rating = request.form['new_rating']
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('edit.html', book=book_to_edit)
 
 
 @app.route("/add", methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
         new_book = Book(
-            title=request.form['name'],
+            title=request.form['title'],
             author=request.form['author'],
-            rating=request.form['rate']
+            rating=request.form['rating'],
         )
-        with app.app_context():
-            db.session.add(new_book)
-            db.session.commit()
+        db.session.add(new_book)
+        db.session.commit()
+        return redirect(url_for('home'))
     return render_template('add.html')
 
 
-@app.route("/edit<int:num>", methods=['GET', 'POST'])
-def edit(num, methods=['GET', 'POST']):
-    if request.method == 'POST':
-        new_rating = request.form['rating']
-        with app.app_context():
-            book_id = num
-            book_to_update = Book.query.get(book_id)
-            # Cathe not input error
-            try:
-                book_to_update.rating = new_rating
-                db.session.commit()
-            except:
-                pass
-        return redirect(url_for('home'))
-    target_book = Book.query.filter_by(id=num).first()
-    return render_template('edit.html', book=target_book)
-
-
-@app.route("/delete<int:num>", methods=['GET', 'POST'])
-def delete(num):
-    with app.app_context():
-        book_id = num
-        book_to_delete = Book.query.get(book_id)
-        db.session.delete(book_to_delete)
-        db.session.commit()
+@app.route("/delete<int:id>", methods=['GET', 'POST'])
+def delete(id):
+    book_to_delete = Book.query.get(id)
+    db.session.delete(book_to_delete)
+    db.session.commit()
     return redirect(url_for('home'))
-
 
 if __name__ == "__main__":
     app.run(debug=True)
